0f3d9683dcbf7f7c3fdea34fe3fb7f0db7971145
DIDRICH Damien <89045920+skunkoff1@users.noreply.github.com>
2022-01-23 14:38:16 +0100

    update gitignore and heroku config

--- api.js
+++ api.js
@@ -19,4 +19,4 @@ app.get("/getWord", (req, res) => {
     });
 });
 
-app.listen(process.env.PORT, () => console.log('Server started: 3000'));
+app.listen(process.env.PORT, () => console.log('Server started: '+ process.env.PORT));
